6119e7cc4dd732dfd82e0a447bd84f8870090e02
Naveen Soni <86905136+naveen-6802@users.noreply.github.com>
2023-01-02 23:10:38 +0530

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,6 +1,12 @@
-//In URL("https://ramenameld.com"), change it to URL(window.location.href) to view address of current tab opened URL.
+async function getCurrentTab() {
+    let queryOptions = {active: true, lastFocusedWindow: true};
+    let [tab] = await chrome.tabs.query(queryOptions);
+    return tab;
+}
+
 async function fetchData() {
-    let domain = (new URL("https://ramenameld.com"));
+    let tab = await getCurrentTab();
+    let domain = (new URL(tab.url));
     domain = domain.hostname;
     console.log("Get doge address for " + domain);
 
